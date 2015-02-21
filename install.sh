49da4a39257dc694b77c84bab9359ca8552a854b
Eric Williamson <eric@williamson.vg>
2015-02-20 21:03:51 -0500

    remove ruby dependencies

--- install.sh
+++ install.sh
@@ -5,10 +5,8 @@
 # A simple bash script for setting up
 # an Operating System with my dotfiles
 
-set OSPACKMAN=''
-
 # Function to determine package manager
-function os_type() {
+function determine_package_manager() {
   which yum > /dev/null && {
     echo "yum"
     export OSPACKMAN="yum"
@@ -26,33 +24,80 @@ function os_type() {
   }
 }
 
+# Adds a symbolic link to files in ~/.dotfiles
+# to your home directory.
+function symlink_files() {
+  ignoredfiles=(LICENSE readme.md install.sh update-zsh.sh zsh-theme vim-colors)
+
+  for f in $(ls -d *); do
+    if [[ ${ignoredfiles[@]} =~ $f ]]; then
+      echo "Skipping $f ..."
+    else
+        link_file $f
+    fi
+  done
+}
+
+# symlink a file
+# arguments: filename
+function link_file(){
+  echo "linking ~/.$1"
+  if ! $(ln -s "$PWD/$1" "$HOME/.$1");  then
+    echo "Replace file '~/.$1'?"
+    read -p "[Y/n]?: " Q_REPLACE_FILE
+    if [[ $Q_REPLACE_FILE != 'n' ]]; then
+      replace_file $1
+    fi
+  fi
+}
+
+# replace file
+# arguments: filename
+function replace_file() {
+  echo "replacing ~/.$1"
+  ln -sf "$PWD/$1" "$HOME/.$1"
+}
+
+function setup_git() {
+  echo 'Setting up git config...'
+  read -p 'Enter Github username: ' GIT_USER
+  git config --global user.name "$GIT_USER"
+  read -p 'Enter email: ' GIT_EMAIL
+  git config --global user.email $GIT_EMAIL
+  git config --global core.editor vim
+  git config --global color.ui true
+  git config --global color.diff auto
+  git config --global color.status auto
+  git config --global color.branch auto
+  # extras
+  git config --global alias.lg log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)%an%Creset' --abbrev-commit
+}
+
 echo "Setting up Operating System..."
 
 set -e
 (
-  os_type
+  determine_package_manager
 
   # general package array
   declare -a packages=('vim' 'git' 'tree' 'htop' 'wget' 'curl')
-
-  # this might get messy if more automated services come into play
-  if [[ $MUTT_INSTALL == "true" ]]; then
-    packages=("${packages[@]}" "mutt")
-  fi
 
   if [[ $OSPACKMAN == "homebrew" ]]; then
     echo "You are running homebrew."
     echo "Using Homebrew to install packages..."
     brew update
-    brew install ${packages[@]} the_silver_searcher
+    declare -a macpackages=('findutils' 'macvim' 'the_silver_searcher')
+    brew install "${packages[@]}" "${macpackages[@]}"
   elif [[ "$OSPACKMAN" == "yum" ]]; then
     echo "You are running yum."
     echo "Using apt-get to install packages...."
-    sudo yum install ${packages[@]} rake zsh
+    sudo yum update
+    sudo yum install "${packages[@]}" zsh
   elif [[ "$OSPACKMAN" == "aptget" ]]; then
     echo "You are running apt-get"
     echo "Using apt-get to install packages...."
-    sudo apt-get install ${packages[@]} rake zsh
+    sudo apt-get update
+    sudo apt-get install "${packages[@]}" zsh
   else
     echo "Could not determine OS. Exiting..."
     exit 1
@@ -61,15 +106,15 @@ set -e
   echo "Installing oh-my-zsh"
   source update-zsh.sh
   echo "Installing dotfiles"
-  rake install
-  mkdir -p ~/.vim/colors
-  cp vim-colors/256_jungle.vim ~/.vim/colors/256_jungle.vim
+  symlink_files
   echo "Installing vim vundles..."
   vim +BundleInstall +qall
+  echo "Installing vim-colors"
+  $(ln -s "$PWD/vim-colors" "$HOME/.vim/colors")
   echo "Changing shells to ZSH"
   chsh -s /bin/zsh
-  echo "Reloading session"
-  exec zsh
 
   echo "Operating System setup complete."
+  echo "Reloading session"
+  exec zsh
 )
